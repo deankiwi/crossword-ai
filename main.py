36d59149ac1b3659e66acd3059eaf4f1fdb9d33c
Dean <dean.w1993@gmail.com>
2025-04-23 22:57:41 +0200

    moved ai functionality to own file

--- main.py
+++ main.py
@@ -1,61 +1,24 @@
-import json
-import requests
-
-# TODO add in cross word generator files
-
-
-def similar_words_finder(word: str, maxLength: int) -> list:
-    prompt = f"""
-return an array of words no greater than {maxLength} letters each related to the word '{word}'
-
-your response should only be a single array:
-"""
-    try:
-        words = eval(message_ai(prompt))
-        return words
-    except SyntaxError as e:
-        print(f"Invalid syntax: {e}")
-
-    return []
-
-
-def clue_for_word(word: str) -> str:
-    # TODO update to stop AI from giving me a long response
-    prompt = f"""Return a small clue for the word '{word}' that can be used in a crossword puzzle"""
-    try:
-        words = message_ai(prompt)
-        return words
-    except SyntaxError as e:
-        print(f"Invalid syntax: {e}")
-
-    return ''
-
-
-def message_ai(prompt: str):
-    url = "http://localhost:11434/api/generate"
-    data = {"prompt": prompt, "model": "llama3.2", "stream": False}
-
-    # Optional: If authentication is needed
-    headers = {
-        "Content-Type": "application/json",
-    }
-
-    # Make the POST request to the LLM
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-
-    # Check the response
-    if response.status_code == 200:
-        # print("Response from LLM:", response.text)
-        data = response.json()
-        print("Response from LLM:", data["response"])
-        return data["response"]
-    else:
-        print(f"Error: {response.status_code} - {response.text}")
+from src.crossword_generator import create_crossword, board_input
+import sys
 
 
 def main():
-    clue_for_word('apple')
+    # input_content = sys.stdin.read()
+
+    # print(input_content.split('\n'))
+    board_input()
+    # create_crossword(5, 5, False)
+
 
 
 if __name__ == "__main__":
+
     main()
+
+# TODO allow user to create any shape crossword, could do this from a text file passed into it with X (can't go) and O (Open for letters)
+# TODO Add two more letters for input, ignore up/down, ignore left/right
+# TODO can also make words down down up and right to left
+# TODO ignore 1 word cross words
+# TODO add website UI
+# TODO get a list of proper nouns
+# TODO add AFD program to Garmin workout to my Notion
